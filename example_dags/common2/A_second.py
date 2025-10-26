f8db197af6ca5a38b952c0762e49c03150af16e0
TadakaKamio <55869030+TadakaKamio@users.noreply.github.com>
2025-10-26 13:10:05 +0900

    Create A_second.py

---
+++ example_dags/common2/A_second.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+dag_id = "A_second_dag_from_config_dag_folder"
+
+with DAG(dag_id=dag_id, start_date=datetime(2018, 11, 14),
+         schedule_interval=None) as dag:
+
+    def say_hello():
+        print("Hello, guys! Your are the best!")
+
+    PythonOperator(task_id="say_hello", python_callable=say_hello)

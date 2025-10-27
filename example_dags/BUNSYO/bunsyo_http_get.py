eb7aef48f51a7fbaa9a5537ff69df5b0fe38c210
TadakaKamio <55869030+TadakaKamio@users.noreply.github.com>
2025-10-27 22:53:36 +0900

    Update bunsyo_http_get.py

--- example_dags/BUNSYO/bunsyo_http_get.py
+++ example_dags/BUNSYO/bunsyo_http_get.py
@@ -15,6 +15,7 @@ from BUNSYO.logger import logger
 #from BUNSYO.bunsyo_http_get_postprocess import *
 
 variables = Variable.get('BUNSYO_HTTP_GET',deserialize_json=True)
+print(variables)
 
 # DAGのスペック設定
 cpu_req = cpu_lim = variables.get('cpu_limit') # 480m

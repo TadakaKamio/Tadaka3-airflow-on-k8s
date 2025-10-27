99869104150ea26c918f8c643d36f39df777ed8f
TadakaKamio <55869030+TadakaKamio@users.noreply.github.com>
2025-10-27 22:54:38 +0900

    Update bunsyo_http_get.py

--- example_dags/BUNSYO/bunsyo_http_get.py
+++ example_dags/BUNSYO/bunsyo_http_get.py
@@ -13,6 +13,7 @@ from urllib.parse import unquote
 from BUNSYO.logger import logger
 #from BUNSYO.bunsyo_http_get_preprocess import *
 #from BUNSYO.bunsyo_http_get_postprocess import *
+import json
 
 variables = Variable.get('BUNSYO_HTTP_GET',deserialize_json=True)
 print(variables)

e20a884600018f8965543c835698c8cf99fe1969
TadakaKamio <55869030+TadakaKamio@users.noreply.github.com>
2025-10-27 22:41:40 +0900

    Update P006_unload_dag.py

--- example_dags/P006_unload/P006_unload_dag.py
+++ example_dags/P006_unload/P006_unload_dag.py
@@ -7,8 +7,8 @@ from common.log import MyAppLog
 logger = MyAppLog()
 
 # DAGのスペック設定
-cpu_req = cpu_lim = Variable.get("P006_cpu_limit",0) # 500m
-ram_req = ram_lim = Variable.get("P006_memory_limit",0) # 3G
+cpu_req = cpu_lim = Variable.get("P006_cpu_limit",500m) # 500m
+ram_req = ram_lim = Variable.get("P006_memory_limit",3G) # 3G
 
 pod_config = {
     "pod_override":

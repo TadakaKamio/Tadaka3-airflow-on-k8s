180b33d8c73006b7c250757d88d39ccb46762a74
TadakaKamio <55869030+TadakaKamio@users.noreply.github.com>
2025-10-27 22:50:10 +0900

    Update bunsyo_http_get.py

--- example_dags/BUNSYO/bunsyo_http_get.py
+++ example_dags/BUNSYO/bunsyo_http_get.py
@@ -14,10 +14,10 @@ from BUNSYO.logger import logger
 #from BUNSYO.bunsyo_http_get_preprocess import *
 #from BUNSYO.bunsyo_http_get_postprocess import *
 
-variables = Variable.get('BUNSYO_HTTP_GET',default_var=0,deserialize_json=True)
+variables = Variable.get('BUNSYO_HTTP_GET',deserialize_json=True)
 
 # DAGのスペック設定
-cpu_req = cpu_lim = variables.get('cpu_limit') # 480m
+cpu_req = cpu_lim = variables.get('cpu_limit',default_var=none) # 480m
 ram_req = ram_lim = variables.get('memory_limit') # 3G
 
 pod_config = {

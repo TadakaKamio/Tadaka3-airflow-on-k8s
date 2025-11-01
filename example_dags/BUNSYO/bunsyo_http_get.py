9acca8673aef212c3972824854b80777c0570399
TadakaKamio <55869030+TadakaKamio@users.noreply.github.com>
2025-11-01 20:01:29 +0900

    Update bunsyo_http_get.py

--- example_dags/BUNSYO/bunsyo_http_get.py
+++ example_dags/BUNSYO/bunsyo_http_get.py
@@ -16,7 +16,7 @@ from BUNSYO.bunsyo_http_get_postprocess import *
 #import json
 
 
-variables = Variable.get("BUNSYO_HTTP_GET",deserialize_json=True)
+variables = Variable.get('BUNSYO_HTTP_GET',deserialize_json=True)
 print(variables)
 
 # DAGのスペック設定

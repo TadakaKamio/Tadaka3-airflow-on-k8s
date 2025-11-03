a8b3fbf95e9d7d05fbb159be1ef13aa00267a7b4
TadakaKamio <55869030+TadakaKamio@users.noreply.github.com>
2025-11-04 05:53:51 +0900

    Update bunsyo_http_get.py

--- example_dags/BUNSYO/bunsyo_http_get.py
+++ example_dags/BUNSYO/bunsyo_http_get.py
@@ -16,8 +16,7 @@ from BUNSYO.bunsyo_http_get_postprocess import *
 import json
 
 
-#variables = Variable.get("BUNSYO_HTTP_GET", deserialize_json=True)
-variables = Variable.get('/usr/local/airflow/dags/gitdags/example_dags/BUNSYO/conf/HTTP_GET/variables_for_DEV.json', deserialize_json=True)
+variables = Variable.get("BUNSYO_HTTP_GET", deserialize_json=True)
 #print(variables["cpu_limit"])
 print(dir(variables))
 

ed035ffd6f9c28158b624d3c335e802957491217
TadakaKamio <55869030+TadakaKamio@users.noreply.github.com>
2025-11-04 05:51:40 +0900

    Update bunsyo_http_get.py

--- example_dags/BUNSYO/bunsyo_http_get.py
+++ example_dags/BUNSYO/bunsyo_http_get.py
@@ -17,7 +17,7 @@ import json
 
 
 #variables = Variable.get("BUNSYO_HTTP_GET", deserialize_json=True)
-variables = Variable.get("BUNSYO/conf/HTTP_GET/variables_for_DEV.json", deserialize_json=True)
+variables = Variable.get("variables_for_DEV.json", deserialize_json=True)
 #print(variables["cpu_limit"])
 print(dir(variables))
 
